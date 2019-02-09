7b65f9c856e1ded58e8df8ae160885e7196dac49
vlkrrrr <yakityyakyak@outlook.com>
2019-02-09 20:54:20 +0100

    base layout

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -19,7 +19,11 @@ export class DataService {
     {id: '1', name: 'Gartenbau', type:'Blumenhandel', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_florist'},
     {id: '2', name: 'Hairf', type:'Friseur', tel: '234', fax: '674456' , email:'s234@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'shopping_basket'},
     {id: '3', name: 'ApoHirsc', type:'Apotheke', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf' , symbol:'hearing'},
-    {id: '4', name: 'Fashion', type:'Mode', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_pharmacy'}
+    {id: '4', name: 'Fashion', type:'Mode', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_pharmacy'},
+    {id: '5', name: 'Gartenbau', type:'Blumenhandel', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_florist'},
+    {id: '6', name: 'Hairf', type:'Friseur', tel: '234', fax: '674456' , email:'s234@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'shopping_basket'},
+    {id: '7', name: 'ApoHirsc', type:'Apotheke', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf' , symbol:'hearing'},
+    {id: '8', name: 'Fashion', type:'Mode', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_pharmacy'}
   ]
   public baseData = new BaseData();
 
