1ae9af052b43d293f2ed3b57dba0ee8e243d72c9
vlkrrrr <yakityyakyak@outlook.com>
2019-02-23 21:40:17 +0100

    data service added

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,12 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Shop } from './shop';
+import { Observable } from 'rxjs';
+import { catchError, map} from 'rxjs/operators';
 
 // remove -example from filename
-class BaseData {
-  welcome = 'CADDELN ◆ CAFFÉ';
-  impTel =  '23423';
-  impEmail = 'bla@bla.de';
-}
 
 
 
@@ -15,18 +13,41 @@ class BaseData {
   providedIn: 'root'
 })
 export class DataService {
-  public shops: Shop[] = [
-    {id: '1', name: 'Gartenbau', type:'Blumenhandel', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_florist'},
-    {id: '2', name: 'Hairf', type:'Friseur', tel: '234', fax: '674456' , email:'s234@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'shopping_basket'},
-    {id: '3', name: 'ApoHirsc', type:'Apotheke', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf' , symbol:'hearing'},
-    {id: '4', name: 'Fashion', type:'Mode', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_pharmacy'},
-    {id: '5', name: 'Gartenbau', type:'Blumenhandel', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_florist'},
-    {id: '6', name: 'Hairf', type:'Friseur', tel: '234', fax: '674456' , email:'s234@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'shopping_basket'},
-    {id: '7', name: 'ApoHirsc', type:'Apotheke', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf' , symbol:'hearing'},
-    {id: '8', name: 'Fashion', type:'Mode', tel: '54542472', fax: '234234234' , email:'sdfsdf@ddd.de', website: 'www.google.de', openhours:'09:00 - 13:00, 14:00 - 18:00', note:'sdfsdf', symbol:'local_pharmacy'}
-  ]
-  public baseData = new BaseData();
+  dataUrl = 'assets/data/production.json'
+  ccdata: Ccdata;
 
-  constructor() { }
+  baseData: BaseData = {
+    welcome: 'Initial Title'
+  }
+  shops: Shop[] = [{id: 1, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 2, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 3, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 4, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 5, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 6, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 7, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 8, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''},
+  {id: 9, name: '', type:'', tel: '', fax: '' , email: '', website: '', openhours:[], note:'', symbol:'', showlogo: false, logopath:''}
+];
+  
+  getCenterData() {
+    return this.http.get<Ccdata>(this.dataUrl);
+  }
+
+constructor(private http: HttpClient) {
+  this.getCenterData()
+    .subscribe((data: Ccdata) => { this.ccdata = {...data},
+    this.shops = this.ccdata.shops;
+    this.baseData = this.ccdata.baseData;
+  })
+ }
 }
 
+interface Ccdata {
+  baseData: BaseData;
+  shops: Shop[];
+}
+
+interface BaseData {
+  welcome: string;
+}
